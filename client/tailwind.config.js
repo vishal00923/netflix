0002fc6d73f2e2877f727532c4f8c31abb7c961f
Vishal Chaurasia <vishalbg.mz@gmail.com>
2022-11-10 03:38:19 +0530

    update : modified configus and add dependency

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    screens: {
+      xs: '412px',
+      sm: '524px',
+      md: '712px',
+      lg: '924px',
+      xl: '1124px',
+      '2xl': '1324px',
+    },
   },
   plugins: [],
 };
