81b8d62df635480a396b28b2bf97f8ccc699e8d8
Vishal Chaurasia <vishalbg.mz@gmail.com>
2022-11-17 03:40:43 +0530

    update : config files

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,13 +3,17 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': `linear-gradient(0deg, rgba(0,0,0,.8) 0, transparent 60%, rgba(0,0,0,.8)), url('https://assets.nflxext.com/ffe/siteui/vlv3/5aecc44d-2a1f-4313-8399-98df20908b64/4d9d5abf-07d8-4dd6-9899-a96b902312ee/IN-en-20221114-popsignuptwoweeks-perspective_alpha_website_large.jpg')`,
+      },
+    },
+
     screens: {
-      xs: '412px',
-      sm: '524px',
-      md: '712px',
-      lg: '924px',
-      xl: '1124px',
-      '2xl': '1324px',
+      mobile: '420px',
+      tablet: '620px',
+      laptop: '1024px',
+      desktop: '1280px',
     },
   },
   plugins: [],
