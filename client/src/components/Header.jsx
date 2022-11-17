a57e788874294fd043ecc0f177193b87d6eba112
Vishal Chaurasia <vishalbg.mz@gmail.com>
2022-11-17 20:14:02 +0530

    update : Nav and Header components

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,12 @@
 import Nav from './Nav';
+import Intro from './Intro';
 
 export default function Header() {
   return (
-    <header className='w-full h-[55vh] bg-hero-pattern border-b-8 border-b-[#202020] bg-center bg-cover tablet:h-[75vh] laptop:h-[80vh]'>
+    <header className='w-full h-[50%] bg-hero-pattern border-b-8 border-b-[#202020] bg-center bg-cover tablet:h-[75vh] laptop:h-[80vh]'>
       <div className='w-full h-full bg-black/40'>
         <Nav />
+        <Intro />
       </div>
     </header>
   );
--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -3,10 +3,10 @@ import logo from '../assets/logo.svg';
 export default function Nav() {
   return (
     <nav
-      className='px-5 pt-5 w-full h-[65px] 
-    tablet:px-10 tablet:pt-6 tablet:h-[80px] laptop:px-9 desktop:px-12'
+      className='px-5 pt-4 w-full h-[65px] 
+    tablet:px-10 tablet:pt-5 tablet:h-[80px] laptop:px-9 desktop:px-12'
     >
-      <div className='flex justify-between items-end pt-2'>
+      <div className='flex justify-between items-center space-x-6 pt-2'>
         <img
           className='w-[90px] h-[auto] tablet:w-[125px] 
           laptop:w-[150px] desktop:w-[180px]'
@@ -14,11 +14,11 @@ export default function Nav() {
           alt='Netflix'
         />
 
-        <div className='flex items-center space-x-3 tablet:space-x-6 laptop:space-x-7 desktop:space-x-8'>
+        <div className='flex items-center pb-[12px] space-x-3 tablet:space-x-6 laptop:space-x-7 desktop:space-x-8'>
           <select
-            className='bg-black text-white tracking-tighter px-1 py-1
-             outline outline-[#ffffffe0] outline-1 text-[0.875rem] 
-             font-semibold tablet:py-1.5 tablet:text-[0.938rem] laptop:py-2 laptop:px-2 desktop:text-[1rem]'
+            className='bg-black text-white px-1 py-[2px]
+             outline outline-gray-400 outline-1 rounded-sm text-[0.875rem] 
+             font-normal tablet:py-[4.5px] tablet:text-[0.938rem] laptop:py-[5px] laptop:px-2 desktop:text-[1rem]'
             name='lang'
             defaultValue='English'
           >
@@ -31,9 +31,9 @@ export default function Nav() {
           </select>
 
           <button
-            className='bg-red-600 text-white tracking-tighter inline-block 
-            px-2 py-1 text-[0.8rem] rounded-sm font-semibold tablet:px-3 
-            tablet:py-1.5 tablet:text-[0.9rem] laptop:text-[1rem] transition-all hover:bg-red-400'
+            className='w-[65px] bg-red-600 text-white tracking-tighter inline-block 
+            px-2 py-1 text-[0.875rem] rounded-sm font-normal tablet:px-3 
+            tablet:py-1.5 tablet:text-[0.9rem] tablet:w-[80px] laptop:text-[1rem] laptop:w-[90px] transition-all hover:bg-red-500'
           >
             Sign In
           </button>
