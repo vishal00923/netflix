4ed01c92378aeef53d0db7e33c1be581d0f54b3a
Vishal Chaurasia <vishalbg.mz@gmail.com>
2022-11-18 19:33:27 +0530

    update : components

--- client/src/components/StoryTV.jsx
+++ client/src/components/StoryTV.jsx
@@ -2,10 +2,10 @@ import tv from '../assets/tv.png';
 
 export default function StoryTV() {
   return (
-    <section className='w-full border-b-8 border-b-[#202020]'>
-      <div className='max-w-[1224px] my-0 mx-auto py-10 tablet:py-12 laptop:py-0 laptop:flex laptop:justify-center laptop:items-center laptop:space-x-20 desktop:space-x-32'>
-        <div className='flex flex-col items-center px-5 laptop:items-start'>
-          <h2 className='text-white text-[1.65rem] mb-3 tablet:text-[2.5rem] laptop:text-[3.25rem]'>
+    <section className='w-full h-full border-b-8 border-b-[#202020]'>
+      <div className='max-w-[1224px] my-0 mx-auto py-10 laptop:flex laptop:justify-center laptop:items-center laptop:space-x-20 desktop:space-x-32'>
+        <div className='flex flex-col items-center space-y-2.5 px-5 laptop:items-start'>
+          <h2 className='text-white text-center text-[1.65rem] tablet:text-[2.5rem] laptop:text-[3.25rem] laptop:text-left desktop:max-w-[600px]'>
             Enjoy on your TV.
           </h2>
           <p className='text-white max-w-[455px] text-center text-[1.125rem] tablet:w-full tablet:font-semibold laptop:text-left laptop:max-w-[380px] laptop:text-[1.45rem] desktop:max-w-[490px] desktop:text-[1.5rem]'>
@@ -14,8 +14,8 @@ export default function StoryTV() {
           </p>
         </div>
 
-        <div className='mt-0'>
-          <div className='relative max-w-[520px] my-0 mx-auto tablet:max-w-[580px] laptop:max-w-[410px] desktop:max-w-[510px]'>
+        <div className='mt-0 pb-1'>
+          <div className='relative max-w-[520px] my-0 mx-auto tablet:max-w-[580px] laptop:max-w-[410px] desktop:max-w-[520px]'>
             <img className='z-10' src={tv} alt='TV' />
             <div className='-z-10 max-w-[72.5%] absolute top-[20.8%] left-[13.5%]'>
               <video
--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -1,11 +1,15 @@
 import Header from '../components/Header';
 import StoryTV from '../components/StoryTV';
+import StoryWatch from '../components/StoryWatch';
+import StoryDownload from '../components/StoryDownload';
 
 export default function Home() {
   return (
     <>
       <Header />
       <StoryTV />
+      <StoryDownload />
+      <StoryWatch />
     </>
   );
 }
