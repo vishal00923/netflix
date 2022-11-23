7da16c14bf561d015208afd4205e5bbd43152386
Vishal Chaurasia <vishalbg.mz@gmail.com>
2022-11-23 22:31:54 +0530

    update :

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 
 import Home from './routes/Home';
+import LogIn from './routes/LogIn';
 
 export default function App() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path='/login' element={<LogIn />} />
     </Routes>
   );
 }
