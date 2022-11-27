7e941346afc616693e3e7957caaf2da11acaf01e
Vishal Chaurasia <vishalbg.mz@gmail.com>
2022-11-27 21:34:44 +0530

    update : components

--- client/src/components/StoryFooter.jsx
+++ client/src/components/StoryFooter.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-
 import Footer from './Footer';
 import Select from './Select';
 
--- client/src/routes/LogIn.jsx
+++ client/src/routes/LogIn.jsx
@@ -1,43 +1,50 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import logo from '../assets/logo.svg';
+
 import Footer from '../components/Footer';
 import Select from '../components/Select';
 
 export default function LogIn() {
   const [toggle, setToggle] = useState('hidden');
-  const [isChecked, setIsChecked] = useState(false);
+  const [isChecked, setIsChecked] = useState(true);
 
   const navigate = useNavigate();
 
+  const handleForm = (event) => event.preventDefault();
+
   return (
     <div className='w-full h-full'>
-      <div className='max-w-[1224px] max-h-[70vh] mx-auto px-4 pt-3 pb-10 border-b-[1px] border-[#888888]'>
+      <div className='max-w-[1224px] mx-auto px-4 pt-3 pb-10 border-b-[1px] border-[#888888] tablet:px-6'>
         <img className='w-[165px] h-auto' src={logo} alt='Netflix' />
 
         <div className='px-1 py-8'>
           <h2 className='text-white text-[2.125rem] font-bold pl-2'>Sign In</h2>
-          <form className='px-1.5 py-5 flex flex-col justify-center space-y-4'>
+          <form
+            onSubmit={handleForm}
+            className='px-1.5 py-5 flex flex-col justify-center space-y-4'
+          >
             <div className='relative'>
               <input
-                className='peer outline-none caret-white w-full h-14 bg-[#333333] placeholder-transparent rounded-[4px] px-5 placeholder:text-[0.875rem] placeholder:font-semibold focus:bg-[#454545]'
+                className='peer pt-3.5 outline-none caret-white w-full h-[52px] bg-[#333333] placeholder-transparent rounded-[4px] px-5 placeholder:text-[0.875rem] placeholder:font-semibold focus:bg-[#454545]'
                 type='email'
                 placeholder='Email or phone number'
+                required
               />
-              <label className='transition-all text-[#888888] text-[0.875rem] absolute peer-placeholder-shown:top-[18px] peer-placeholder-shown:left-5 peer-focus:top-1.5 peer-focus:left-5 peer-focus:text-[0.688rem] peer-focus:font-normal'>
+              <label className='transition-all text-[#888888] text-[0.875rem] absolute peer-placeholder-shown:top-4 peer-placeholder-shown:left-5 peer-focus:top-1.5 peer-focus:left-5 peer-focus:text-[10.8px] peer-focus:font-normal peer-focus:pb-1'>
                 Email or phone number
               </label>
             </div>
 
             <div className='relative'>
               <input
-                className='peer outline-none caret-white w-full h-14 bg-[#333333] placeholder-transparent rounded-[4px] px-5 placeholder:text-[0.875rem] placeholder:font-semibold focus:bg-[#454545]'
+                className='peer pt-3.5 outline-none caret-white w-full h-[52px] bg-[#333333] placeholder-transparent rounded-[4px] px-5 placeholder:text-[0.875rem] placeholder:font-semibold focus:bg-[#454545]'
                 type='email'
                 placeholder='Password'
+                required
               />
-              <label className='transition-all text-[#888888] text-[0.875rem] absolute peer-placeholder-shown:top-[18px] peer-placeholder-shown:left-5 peer-focus:top-1.5 peer-focus:left-5 peer-focus:text-[0.688rem] peer-focus:font-normal'>
+              <label className='transition-all text-[#888888] text-[0.875rem] absolute peer-placeholder-shown:top-4 peer-placeholder-shown:left-5 peer-focus:top-1.5 peer-focus:left-5 peer-focus:text-[10.8px] peer-focus:font-normal'>
                 Password
               </label>
             </div>
@@ -51,7 +58,7 @@ export default function LogIn() {
             <div className='flex justify-between items-center'>
               <div className='flex items-center space-x-1'>
                 <input
-                  onClick={() => setIsChecked((prev) => !prev)}
+                  onChange={() => setIsChecked((prev) => !prev)}
                   className='w-4 h-4'
                   type='checkbox'
                   checked={isChecked}
@@ -80,8 +87,7 @@ export default function LogIn() {
             </p>
             <p className='text-[#888888] text-[12.5px] pb-5 w-full'>
               This page is protected by Google reCAPTCHA to ensure you're not a
-              bot.
-              <br />
+              bot.{' '}
               <span
                 onClick={() => setToggle('')}
                 className={`${
@@ -92,13 +98,11 @@ export default function LogIn() {
               </span>
             </p>
             <div
-              className={`${
-                toggle === 'hidden'
-                  ? 'overflow-hidden h-0 transition-all'
-                  : 'h-auto transition-all'
-              } max-w-[438px] my-1`}
+              className={`transition-all ${
+                toggle === 'hidden' ? 'overflow-hidden h-0' : 'h-5'
+              }`}
             >
-              <p className='text-[#8c8c8c] text-[13px] leading-[14px]'>
+              <p className='max-w-[438px] text-[#8c8c8c] text-[13px] leading-[14px] tablet:leading-[16px] tablet:max-w-[618px]'>
                 The information collected by Google reCAPTCHA is subject to the
                 Google{' '}
                 <a
@@ -125,7 +129,7 @@ export default function LogIn() {
         </div>
       </div>
 
-      <div className='max-w-[1224px] mx-auto text-[#8b8b8b] px-[25px] pt-8 pb-14'>
+      <div className='max-w-[1224px] mx-auto text-[#8b8b8b] px-6 pt-8 pb-14 tablet:px-8'>
         <Footer />
         <Select />
       </div>
