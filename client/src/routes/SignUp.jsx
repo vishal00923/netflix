a153bf41e6236b20c6a807f69851c9a127640f00
Vishal Chaurasia <vishalbg.mz@gmail.com>
2022-12-05 23:22:31 +0530

    add : SignUp route

---
+++ client/src/routes/SignUp.jsx
@@ -0,0 +1,21 @@
+import FAQ from '../components/FAQ';
+import Header from '../components/Header';
+import StoryFooter from '../components/StoryFooter';
+import StoryTV from '../components/StoryTV';
+import StoryWatch from '../components/StoryWatch';
+import StoryDownload from '../components/StoryDownload';
+import StoryChildren from '../components/StoryChildren';
+
+export default function SignUp() {
+  return (
+    <>
+      <Header />
+      <StoryTV />
+      <StoryDownload />
+      <StoryWatch />
+      <StoryChildren />
+      <FAQ />
+      <StoryFooter />
+    </>
+  );
+}
