7901a5e009a39c89141dc455ef05bfaec2ebe8bf
Vishal Chaurasia <vishalbg.mz@gmail.com>
2022-12-06 23:31:55 +0530

    update : packages

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -10,7 +10,6 @@ module.exports = {
         'login-screen': `linear-gradient(0deg, rgba(0,0,0,.2) 0, transparent 90%, rgba(0,0,0,.4)), url('https://assets.nflxext.com/ffe/siteui/vlv3/e89c9679-2f5f-491d-924c-c58924a8ee4b/8ec27a1d-02ce-4489-b320-a95106906f5d/IN-en-20221121-popsignuptwoweeks-perspective_alpha_website_large.jpg')`,
       },
     },
-
     screens: {
       xs: '398px',
       login: '740px',
@@ -20,5 +19,5 @@ module.exports = {
       desktop: '1280px',
     },
   },
-  plugins: [],
+  plugins: [require('tailwind-scrollbar-hide')],
 };
