4c6264905157a4c0eae2e2de4668e46d2c3924ba
Vishal Chaurasia <vishalbg.mz@gmail.com>
2022-12-08 22:29:28 +0530

    update : modified requests

--- client/src/api/requests.js
+++ client/src/api/requests.js
@@ -1,10 +1,10 @@
-const api_key = process.env.REACT_APP_TMDB_KEY;
+const API_KEY = process.env.REACT_APP_TMDB_KEY;
 
-export const fetchTrending = `/trending/all/week?api_key=${api_key}`;
-export const fetchNetflixOriginals = `/discover/tv?api_key=${api_key}&with_networks=123&language=en-US`;
-export const fetchTopRated = `/movie/top_rated?api_key=${api_key}&language=en-US&include_adult=true&include_video=true`;
-export const fetchActionMovies = `/discover/movie?api_key=${api_key}&with_genres=28&language=en-US&include_adult=true&include_video=true`;
-export const fetchComedyMovies = `/discover/movie?api_key=${api_key}&with_genres=35&language=en-US&include_adult=true&include_video=true`;
-export const fetchHorrorMovies = `/discover/movie?api_key=${api_key}&with_genres=27&language=en-US&include_adult=true&include_video=true`;
-export const fetchRomanceMovies = `/discover/movie?api_key=${api_key}&with_genres=10749&language=en-US&include_adult=true&include_video=true`;
-export const fetchDocumentaries = `/discover/movie?api_key=${api_key}&with_genres=&language=en-US&include_adult=true&include_video=true`;
+export const fetchTrending = `/trending/all/week?api_key=${API_KEY}&language=en-US`;
+export const fetchNetflixOriginals = `/discover/movie?api_key=${API_KEY}&with_networks=213`;
+export const fetchTopRated = `/movie/top_rated?api_key=${API_KEY}&language=en-US`;
+export const fetchActionMovies = `/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=28`;
+export const fetchComedyMovies = `/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=35`;
+export const fetchHorrorMovies = `/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=27`;
+export const fetchRomanceMovies = `/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=10749`;
+export const fetchDocumentaries = `/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=99`;
